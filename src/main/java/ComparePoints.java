d89b10338cfb3e9b7b1ccfc25957ebb87c033fee
jlm2112 <arcissexempli@gmail.com>
2017-02-02 14:45:11 -0500

    Hi guys

--- src/main/java/ComparePoints.java
+++ src/main/java/ComparePoints.java
@@ -1,42 +1,66 @@
 
 public class ComparePoints {
 
-	public String Compass(String input) {
+	public String findDirection(String input) {
 		String[] direction = input.split(",");
 		// now we have an array of 4 things, first two are O P and second two
 		// are Q R
 		String result = null;
-		if (direction[0].equals(direction[2]) && direction[1].equals(direction[3])) {
+		String fromX = direction[0];
+		String toX = direction[2];
+		String fromY = direction[1];
+		String toY = direction[3];
+		if (fromX.equals(toX) && fromY.equals(toY)) {
 			// if O equals Q and P equals R
 			result = "Here"; // then we're already Here!
-		} else if (direction[0].compareTo(direction[2]) < 0 && direction[1].equals(direction[3])) {
-			// if O is less than Q and P equals R
-			result = "E"; // then we need to go East!
-		} else if (direction[0].compareTo(direction[2]) > 0 && direction[1].equals(direction[3])) {
-			// if O is greater than Q and P equals R
-			result = "W"; // then we need to go West!
-		} else if (direction[0].equals(direction[2]) && direction[1].compareTo(direction[3]) > 0) {
-			// if O is equal to Q and P is greater than R
-			result = "S"; // then we need to go South!
-		} else if (direction[0].equals(direction[2]) && direction[1].compareTo(direction[3]) < 0) {
-			// if O is equal to Q and P is less than R
-			result = "N"; // then we need to go North!
+		} else {
+			boolean movingEast = isLessThan(fromX, toX);
+			if (movingEast && fromY.equals(toY)) {
+				// if O is less than Q and P equals R
+				result = "E"; // then we need to go East!
+			} else {
+				boolean movingWest = isGreaterThan(fromX, toX);
+				if (movingWest && fromY.equals(toY)) {
+					// if O is greater than Q and P equals R
+					result = "W"; // then we need to go West!
+				} else {
+					boolean movingSouth = isGreaterThan(fromY, toY);
+					if (fromX.equals(toX) && movingSouth) {
+						// if O is equal to Q and P is greater than R
+						result = "S"; // then we need to go South!
+					} else {
+						boolean movingNorth = isLessThan(fromY, toY);
+						if (fromX.equals(toX) && movingNorth) {
+							// if O is equal to Q and P is less than R
+							result = "N"; // then we need to go North!
 
-		} else if (direction[0].compareTo(direction[2]) < 0 && direction[1].compareTo(direction[3]) > 0) {
-			// if O is less than Q and P is greater than R
-			result = "SE"; // then we need to go SouthEast!
-		} else if (direction[0].compareTo(direction[2]) > 0 && direction[1].compareTo(direction[3]) > 0) {
-			// if O is greater than Q and P is greater than R
-			result = "SW"; // then we need to go SouthWest!
-		} else if (direction[0].compareTo(direction[2]) < 0 && direction[1].compareTo(direction[3]) < 0) {
-			// if O is less than Q and P is less than R
-			result = "NE"; // then we need to go NorthEast!
-		} else if (direction[0].compareTo(direction[2]) > 0 && direction[1].compareTo(direction[3]) < 0) {
-			// if O is greater than Q and P is less than R
-			result = "NW"; // then we need to go NorthWest!
+						} else if (movingEast && movingSouth) {
+							// if O is less than Q and P is greater than R
+							result = "SE"; // then we need to go SouthEast!
+						} else if (movingWest && movingSouth) {
+							// if O is greater than Q and P is greater than R
+							result = "SW"; // then we need to go SouthWest!
+						} else if (movingEast && movingNorth) {
+							// if O is less than Q and P is less than R
+							result = "NE"; // then we need to go NorthEast!
+						} else if (movingWest && movingNorth) {
+							// if O is greater than Q and P is less than R
+							result = "NW"; // then we need to go NorthWest!
 
+						}
+					}
+				}
+			}
 		}
 		return result;
 
+	}
+
+	private boolean isGreaterThan(String fromX, String toX) {
+		return fromX.compareTo(toX) > 0;
+	}
+
+	private boolean isLessThan(String fromY, String toY) {
+		return fromY.compareTo(toY) < 0;
 	}
 }
